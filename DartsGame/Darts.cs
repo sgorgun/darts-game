a2beb848ee801f44eff60cc243e0010f3d1e8f5e
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-08 09:41:59 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ DartsGame/Darts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DartsGame
+{
+    public static class Darts
+    {
+        /// <summary>
+        /// Calculates the earned points in a single toss of a Darts game.
+        /// </summary>
+        /// <param name="x">x-coordinate of dart.</param>
+        /// <param name="y">y-coordinate of dart.</param>
+        /// <returns>The earned points.</returns>
+        public static int GetScore(double x, double y)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
